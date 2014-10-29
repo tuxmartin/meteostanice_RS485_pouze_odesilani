4881e96b12abde9db99cc93c6349b4bf05fc943c
Martin Vancl <tux.martin@gmail.com>
2014-10-29 23:37:59 +0100

    +meteostanice prepsana na MODBUS (PC v Pythonu)
    Testovano spojeni 1 PC a 1 Arduino.
    Na PC pouzit cinsky USB - RS-485 prevodnik s autoamtickym prepinanim.

--- meteostanice_RS485_pouze_odesilani.ino
+++ meteostanice_RS485_pouze_odesilani.ino
@@ -1,50 +1,68 @@
+#include <SimpleModbusSlave.h>
 #include <DHT.h>
-
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
 
 #define DHTPIN 7
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-
 DHT dht(DHTPIN, DHTTYPE); // DHT22 - vlhkost
+
 Adafruit_BMP085 bmp; // BMP085 - tlak
 
-long interval = 60;  // interval mereni [s]
-uint32_t timer;
 
-unsigned long count = 0;
+#define  ledPin  13 // onboard led
+//////////////// registers of your slave ///////////////////
+enum 
+{     
+  DHT22_TEMP_VAL,  
+  DHT22_HUM_VAL,   
+  DHT22_ERROR,
+  BMP085_TEMP_VAL,        
+  BMP085_PRES_VAL,
+  LED_STATE,
+  MEASURE,
+  HOLDING_REGS_SIZE // leave this one
+};
+unsigned int holdingRegs[HOLDING_REGS_SIZE]; // function 3 and 16 register array
+//////////////// registers of your slave ///////////////////
 
 void setup () {
-  Serial.begin(9600); 
+  // SERIAL_8N2: 1 start bit, 8 data bits, 2 stop bits
+  modbus_configure(&Serial, 19200, SERIAL_8N2, 2, 2, HOLDING_REGS_SIZE, holdingRegs);
+  
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 
   /* ####### BMP085 ####### */
   if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	//Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
   }
   /* ####### BMP085 ####### */
+  
+  merit();
 }
 
 void loop () {  
-  if (millis() > timer) {
-    timer = millis() + (interval * 1000);
-    
+  modbus_update();
+
+  if (holdingRegs[MEASURE] == 1) { // Mam merit?	  
     merit();
-    
+    holdingRegs[MEASURE] = 0;
+    serialFlushBuffer(); // vyprazdime buffer - to bude delat bordel, pokud budou aktualne prichazet data...
   }
+  // read the LED_STATE register value and set the onboard LED high or low with function 16
+  if (holdingRegs[LED_STATE] == 1) {	  
+    digitalWrite(ledPin, HIGH);
+    holdingRegs[LED_STATE] = 1;
+  } else {
+    digitalWrite(ledPin, LOW);
+    holdingRegs[LED_STATE] = 0;
+  }
+  
 }
 
-void merit() { 
-  
-  /* Data posilana do PC maji nasledujici format a zakoncena jsou <CR><LF>:
-   *   3;DHT22;23.40C;49.80%;BMP085;22.60C;99581Pa;
-   */
-  
-  //Serial.print(F("Mereni c. "));
-  Serial.print(count);
-  Serial.print(F(";"));
-
-  
+void merit() {
   /* ####### DHT22 ####### */   
   dht.begin();   
   // Reading temperature or humidity takes about 250 milliseconds!
@@ -54,28 +72,21 @@ void merit() {
     
   // check if returns are valid, if they are NaN (not a number) then something went wrong!
   if (isnan(t) || isnan(h)) {    
-    Serial.println(F("DHT22_read_fail"));
+    holdingRegs[DHT22_ERROR] = 1; // DHT22_read_fail
   } else { 
-    Serial.print(F("DHT22;"));
-    Serial.print(t);
-    Serial.print(F("C;"));
-    Serial.print(h);
-    Serial.print(F("%"));
+    holdingRegs[DHT22_ERROR] = 0;
+    holdingRegs[DHT22_TEMP_VAL] = t*10;
+    holdingRegs[DHT22_HUM_VAL] = h*10;
   }
   /* ####### DHT22 ####### */
-  
-  Serial.print(F(";"));
-    
-  /* ####### BMP085 ####### */
-  Serial.print(F("BMP085;"));
-  Serial.print(bmp.readTemperature());
-  Serial.print(F("C;"));
-  Serial.print(bmp.readPressure());
-  Serial.print(F("Pa;"));
-  /* ####### BMP085 ####### */
-  
-  Serial.println(F(""));
 
-  count++;
+  /* ####### BMP085 ####### */
+  holdingRegs[BMP085_TEMP_VAL] = int( bmp.readTemperature() * 10 );
+  holdingRegs[BMP085_PRES_VAL] = ( bmp.readPressure() / 100);
+  /* ####### BMP085 ####### */  
 }
 
+void serialFlushBuffer() {
+  while (Serial.read() >= 0)
+   ; // do nothing
+}
---
+++ pc/pokus.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+# https://pbs.twimg.com/media/B1ERDW9CcAELi4h.png:large
+
+
+# -*- coding: utf-8 -*-
+	# Podpora UTF-8 znaku v souboru.
+	# Musi byt na 1., nebo 2. radku!
+
+# http://minimalmodbus.sourceforge.net/internalminimalmodbus.html
+
+# http://effbot.org/pyfaq/how-do-i-create-a-pyc-file.htm
+# http://code.activestate.com/recipes/576704-python-code-minifier/
+# python -m py_compile fileA.py fileB.py fileC.py
+
+import minimalmodbus
+import time
+from time import sleep
+import serial
+
+MODBUS_3  = 3  # Read holding registers
+MODBUS_16 = 16 # Write multiple registers
+STUPEN    = u'\u00b0' # znak pro stupen
+
+def relativniTlak(absolutniTlak, teplota, nadmorskaVyska):
+	# http://forum.amaterskameteorologie.cz/viewtopic.php?f=9&t=65#p1968
+	g = 9.80665
+	return ((absolutniTlak * g * nadmorskaVyska) / (287 * (273 + teplota) + (nadmorskaVyska / 400))) + absolutniTlak
+
+
+print "---------------------------------------"
+print "Aktualni cas: ",  time.strftime("%Y-%m-%d %H:%M:%S")
+
+"""
+DEFAULT VALUES:
+instrument.serial.port     = '/dev/ttyUSB1'     # this is the serial port name
+instrument.serial.baudrate = 19200   # Baud
+instrument.serial.bytesize = 8
+instrument.serial.parity   = serial.PARITY_NONE
+instrument.serial.stopbits = 1
+instrument.serial.timeout  = 0.05   # seconds
+instrument.address         = 2      # this is the slave address number
+instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
+"""
+
+"""
+instrument.debug = True
+"""
+# SERIAL_8N2: 1 start bit, 8 data bits, 2 stop bits
+dev1 = minimalmodbus.Instrument('/dev/ttyUSB1', 2) # port name, slave address (in decimal)
+dev1.serial.baudrate = 19200
+dev1.serial.bytesize = 8
+dev1.serial.stopbits = 2
+dev1.serial.parity   = serial.PARITY_NONE
+
+"""
+instrumentA = minimalmodbus.Instrument('/dev/ttyUSB1', 1)
+instrumentB = minimalmodbus.Instrument('/dev/ttyUSB1', 2)
+"""
+
+"""
+# registeraddress (int), numberOfDecimals (int), functioncode (int)
+data  =  dev1.read_register(0, 1, MODBUS_3)
+
+#registeraddress (int), numberOfRegisters (int), functioncode (int)
+data = dev1.read_registers(0, 6, MODBUS_3)
+
+
+#registeraddress (int), value (int or float), numberOfDecimals (int), functioncode (int)
+dev1.write_register(5, ledStatus, 1, MODBUS_16)
+
+
+#registeraddress (int), values (list of int)
+dev1.write_registers(5, ledStatus)
+"""
+
+# Posleme prikaz k namereni dat (Arduino si je nameri a ulozi do RAM):
+zmerData = 1
+dev1.write_register(6, zmerData, 0, MODBUS_16)
+sleep(1) # 500ms  - mereni trva asi 250ms
+
+# Precteme namerena data:
+data = dev1.read_registers(0, 6, MODBUS_3)
+print "RAW data: ", data
+"""
+  DHT22_TEMP_VAL,  
+  DHT22_HUM_VAL,   
+  DHT22_ERROR,
+  BMP085_TEMP_VAL,        
+  BMP085_PRES_VAL,
+  LED_STATE,
+  MEASURE,
+"""
+DHT22_TEMP_VAL  = data[0] / 10.0
+DHT22_HUM_VAL   = data[1] / 10.0
+DHT22_ERROR     = data[2]
+BMP085_TEMP_VAL = data[3] / 10.0
+BMP085_PRES_VAL = data[4]
+LED_STATE       = data[5]
+
+relTlak = relativniTlak(BMP085_PRES_VAL, BMP085_TEMP_VAL, 287) # Jicin ma podle Googlu 287mnm
+
+print "DHT22: ", DHT22_TEMP_VAL,  STUPEN, "C ",  DHT22_HUM_VAL, "%"
+print "BMP085 ", BMP085_TEMP_VAL, STUPEN, "C ", BMP085_PRES_VAL, "hPa (absolutni) ",  "{:4.2f}".format(relTlak) , "hPa (relativni)"
+
+# https://docs.python.org/3.3/faq/programming.html#is-there-an-equivalent-of-c-s-ternary-operator   [on_true] if [expression] else [on_false]
+print "Stav LED - ", LED_STATE, " - ", ("svitila - VYPINAM" if LED_STATE else "nesvitila - ZAPINAM")
+
+if LED_STATE == 1:
+	LED_STATE=0
+else:
+	LED_STATE=1
+
+#registeraddress (int), value (int or float), numberOfDecimals (int), functioncode (int)
+sleep(1) # 1s
+dev1.write_register(5, LED_STATE, 0, MODBUS_16)
+
+
+
+print "---------------------------------------"
+
