5b5045ba76764c96d487616fc58cb94b683e7a67
Martin Vancl <tux.martin@gmail.com>
2014-11-06 12:36:15 +0100

    +trvale mereni kazdych X sekund

--- pc/pokus.py
+++ pc/pokus.py
@@ -25,6 +25,8 @@ import urllib2 # posilani na server (REST client)
 
 url = 'http://localhost/meteo/'
 
+frekvenceMereni = 10 # [s]
+
 MODBUS_3  = 3  # Read holding registers
 MODBUS_16 = 16 # Write multiple registers
 STUPEN    = u'\u00b0' # znak pro stupen
@@ -47,116 +49,120 @@ def rosnyBod(teplota, vlhkost):
 	print Tdp
 	"""
 
-print "---------------------------------------"
-print "Aktualni cas: ",  strftime("%Y-%m-%d %H:%M:%S")
+def main():
+	print "---------------------------------------"
+	print "Aktualni cas: ",  strftime("%Y-%m-%d %H:%M:%S")
 
-"""
-DEFAULT VALUES:
-instrument.serial.port     = '/dev/ttyUSB1'     # this is the serial port name
-instrument.serial.baudrate = 19200   # Baud
-instrument.serial.bytesize = 8
-instrument.serial.parity   = serial.PARITY_NONE
-instrument.serial.stopbits = 1
-instrument.serial.timeout  = 0.05   # seconds
-instrument.address         = 2      # this is the slave address number
-instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
-"""
+	"""
+	DEFAULT VALUES:
+	instrument.serial.port     = '/dev/ttyUSB1'     # this is the serial port name
+	instrument.serial.baudrate = 19200   # Baud
+	instrument.serial.bytesize = 8
+	instrument.serial.parity   = serial.PARITY_NONE
+	instrument.serial.stopbits = 1
+	instrument.serial.timeout  = 0.05   # seconds
+	instrument.address         = 2      # this is the slave address number
+	instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
+	"""
 
-"""
-instrument.debug = True
-"""
-# SERIAL_8N2: 1 start bit, 8 data bits, 2 stop bits
-dev1 = minimalmodbus.Instrument('/dev/ttyUSB1', 2) # port name, slave address (in decimal)
-dev1.serial.baudrate = 19200
-dev1.serial.bytesize = 8
-dev1.serial.stopbits = 2
-dev1.serial.parity   = serial.PARITY_NONE
+	"""
+	instrument.debug = True
+	"""
+	# SERIAL_8N2: 1 start bit, 8 data bits, 2 stop bits
+	dev1 = minimalmodbus.Instrument('/dev/ttyUSB1', 2) # port name, slave address (in decimal)
+	dev1.serial.baudrate = 19200
+	dev1.serial.bytesize = 8
+	dev1.serial.stopbits = 2
+	dev1.serial.parity   = serial.PARITY_NONE
 
-"""
-instrumentA = minimalmodbus.Instrument('/dev/ttyUSB1', 1)
-instrumentB = minimalmodbus.Instrument('/dev/ttyUSB1', 2)
-"""
+	"""
+	instrumentA = minimalmodbus.Instrument('/dev/ttyUSB1', 1)
+	instrumentB = minimalmodbus.Instrument('/dev/ttyUSB1', 2)
+	"""
 
-"""
-# registeraddress (int), numberOfDecimals (int), functioncode (int)
-data  =  dev1.read_register(0, 1, MODBUS_3)
+	"""
+	# registeraddress (int), numberOfDecimals (int), functioncode (int)
+	data  =  dev1.read_register(0, 1, MODBUS_3)
 
-#registeraddress (int), numberOfRegisters (int), functioncode (int)
-data = dev1.read_registers(0, 6, MODBUS_3)
-
-
-#registeraddress (int), value (int or float), numberOfDecimals (int), functioncode (int)
-dev1.write_register(5, ledStatus, 1, MODBUS_16)
+	#registeraddress (int), numberOfRegisters (int), functioncode (int)
+	data = dev1.read_registers(0, 6, MODBUS_3)
 
 
-#registeraddress (int), values (list of int)
-dev1.write_registers(5, ledStatus)
-"""
-
-# Posleme prikaz k namereni dat (Arduino si je nameri a ulozi do RAM):
-zmerData = 1
-dev1.write_register(1, zmerData, 0, MODBUS_16) # MEASURE - 1
-sleep(1) # 500ms  - mereni DHT22 trva asi 250ms
-
-# Precteme namerena data:
-data = dev1.read_registers(0, 9, MODBUS_3)
-print "RAW data: ", data
-"""
-  DEVICE_VER,  	0
-  MEASURE,	1
-  BMP085_ERROR,	2
-  DHT22_ERROR,	3
-  DHT22_TEMP,  	4
-  DHT22_HUM,   	5
-  BMP085_TEMP,	6   	     
-  BMP085_PRES,	7
-  LED_STATE,	8
-"""
-DEVICE_VER	= data[0]
-BMP085_ERROR	= data[2]
-DHT22_ERROR	= data[3]
-DHT22_TEMP_VAL  = data[4] / 10.0
-DHT22_HUM_VAL   = data[5] / 10.0
-BMP085_TEMP_VAL = data[6] / 10.0
-BMP085_PRES_VAL = data[7]
-LED_STATE       = data[8]
-
-relTlak = relativniTlak(BMP085_PRES_VAL, BMP085_TEMP_VAL, 287) # Jicin ma podle Googlu 287mnm
-rBod = rosnyBod(DHT22_TEMP_VAL, DHT22_HUM_VAL)
-
-print ""
-print "DHT22: ", DHT22_TEMP_VAL,  STUPEN, "C, ",  DHT22_HUM_VAL, "%, rosny bod ", rBod,  STUPEN, "C" 
-print "BMP085 ", BMP085_TEMP_VAL, STUPEN, "C, ", BMP085_PRES_VAL, "hPa (absolutni), ",  relTlak , "hPa (relativni)"
-print ""
-
-params = urllib.urlencode({ # napleneni dat k odeslani
-  'DEVICE_VER'		: DEVICE_VER,
-  'BMP085_ERROR'	: BMP085_ERROR,
-  'DHT22_ERROR'		: DHT22_ERROR,
-  'DHT22_TEMP_VAL'	: DHT22_TEMP_VAL,
-  'DHT22_HUM_VAL'	: DHT22_HUM_VAL,
-  'DHT22_ROSNY_BOD'	: rBod,
-  'BMP085_TEMP_VAL'	: BMP085_TEMP_VAL,
-  'BMP085_PRES_VAL'	: BMP085_PRES_VAL,
-  'BMP085_REL_TLAK'	: relTlak
-})
-# DHT22_ROSNY_BOD=12.2&BMP085_PRES_VAL=986&DHT22_HUM_VAL=54.0&BMP085_REL_TLAK=1018.81084798&BMP085_TEMP_VAL=21.7&DHT22_TEMP_VAL=21.9
-try:
-    response = urllib2.urlopen(url, params, timeout = 5).read() # poslani na server a precteni odpovedi
-except urllib2.URLError, e:
-    print "Nepovedlo se poslat data na server!"
-
-# https://docs.python.org/3.3/faq/programming.html#is-there-an-equivalent-of-c-s-ternary-operator   [on_true] if [expression] else [on_false]
-print "Stav LED - ", LED_STATE, " - ", ("svitila - VYPINAM" if LED_STATE else "nesvitila - ZAPINAM")
-
-if LED_STATE:
-	LED_STATE=0
-else:
-	LED_STATE=1
-
-dev1.write_register(8, LED_STATE, 0, MODBUS_16)
+	#registeraddress (int), value (int or float), numberOfDecimals (int), functioncode (int)
+	dev1.write_register(5, ledStatus, 1, MODBUS_16)
 
 
+	#registeraddress (int), values (list of int)
+	dev1.write_registers(5, ledStatus)
+	"""
 
-print "---------------------------------------"
+	# Posleme prikaz k namereni dat (Arduino si je nameri a ulozi do RAM):
+	zmerData = 1
+	dev1.write_register(1, zmerData, 0, MODBUS_16) # MEASURE - 1
+	sleep(1) # 500ms  - mereni DHT22 trva asi 250ms
+
+	# Precteme namerena data:
+	data = dev1.read_registers(0, 9, MODBUS_3)
+	print "RAW data: ", data
+	"""
+	  DEVICE_VER,  	0
+	  MEASURE,	1
+	  BMP085_ERROR,	2
+	  DHT22_ERROR,	3
+	  DHT22_TEMP,  	4
+	  DHT22_HUM,   	5
+	  BMP085_TEMP,	6   	     
+	  BMP085_PRES,	7
+	  LED_STATE,	8
+	"""
+	DEVICE_VER	= data[0]
+	BMP085_ERROR	= data[2]
+	DHT22_ERROR	= data[3]
+	DHT22_TEMP_VAL  = data[4] / 10.0
+	DHT22_HUM_VAL   = data[5] / 10.0
+	BMP085_TEMP_VAL = data[6] / 10.0
+	BMP085_PRES_VAL = data[7]
+	LED_STATE       = data[8]
+
+	relTlak = relativniTlak(BMP085_PRES_VAL, BMP085_TEMP_VAL, 287) # Jicin ma podle Googlu 287mnm
+	rBod = rosnyBod(DHT22_TEMP_VAL, DHT22_HUM_VAL)
+
+	print ""
+	print "DHT22: ", DHT22_TEMP_VAL,  STUPEN, "C, ",  DHT22_HUM_VAL, "%, rosny bod ", rBod,  STUPEN, "C" 
+	print "BMP085 ", BMP085_TEMP_VAL, STUPEN, "C, ", BMP085_PRES_VAL, "hPa (absolutni), ",  relTlak , "hPa (relativni)"
+	print ""
+
+	params = urllib.urlencode({ # napleneni dat k odeslani
+	  'DEVICE_VER'		: DEVICE_VER,
+	  'BMP085_ERROR'	: BMP085_ERROR,
+	  'DHT22_ERROR'		: DHT22_ERROR,
+	  'DHT22_TEMP_VAL'	: DHT22_TEMP_VAL,
+	  'DHT22_HUM_VAL'	: DHT22_HUM_VAL,
+	  'DHT22_ROSNY_BOD'	: rBod,
+	  'BMP085_TEMP_VAL'	: BMP085_TEMP_VAL,
+	  'BMP085_PRES_VAL'	: BMP085_PRES_VAL,
+	  'BMP085_REL_TLAK'	: relTlak
+	})
+	# DHT22_ROSNY_BOD=12.2&BMP085_PRES_VAL=986&DHT22_HUM_VAL=54.0&BMP085_REL_TLAK=1018.81084798&BMP085_TEMP_VAL=21.7&DHT22_TEMP_VAL=21.9
+	try:
+	    response = urllib2.urlopen(url, params, timeout = 5).read() # poslani na server a precteni odpovedi
+	except urllib2.URLError, e:
+	    print "Nepovedlo se poslat data na server!"
+
+	# https://docs.python.org/3.3/faq/programming.html#is-there-an-equivalent-of-c-s-ternary-operator   [on_true] if [expression] else [on_false]
+	print "Stav LED - ", LED_STATE, " - ", ("svitila - VYPINAM" if LED_STATE else "nesvitila - ZAPINAM")
+
+	if LED_STATE:
+		LED_STATE=0
+	else:
+		LED_STATE=1
+
+	dev1.write_register(8, LED_STATE, 0, MODBUS_16)
+
+
+	print "---------------------------------------"
+
+while 1:
+	main()
+	sleep(frekvenceMereni)
 
